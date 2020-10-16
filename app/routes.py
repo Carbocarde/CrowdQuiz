5e4eba623728ae0912c8493840eef2ae53534fe0
poneill8 <poneill8@gatech.edu>
2020-10-16 18:19:50 -0400

    add webpage base

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,18 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username':'Patrick'}
+    questions = [
+        {
+            'author': {'username': 'Patrick'},
+            'body': 'What\'s an interval order?'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'When was the battle of Gettysburg?'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, questions=questions)
