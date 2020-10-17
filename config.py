79dc2fe91ddbc36c142a20f70b03f6c3f82f852f
poneill8 <poneill8@gatech.edu>
2020-10-17 10:37:32 -0400

    initial database commit

--- config.py
+++ config.py
@@ -1,4 +1,7 @@
 import os
 
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'youwontguess'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or '33lkjdf#897lk'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
--- env.bat
+++ env.bat
@@ -1,1 +1,1 @@
-start cmd.exe /k "venv\Scripts\activate && setx FLASK_APP crowdquiz.py"
+start cmd.exe /k "venv\Scripts\activate && setx FLASK_APP crowdquiz.py && setx PGPASSWORD dbpwok && setx DATABASE_URL postgresql:///crowdquiz"
